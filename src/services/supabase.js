a02ab2a1f68e15b39c395bf4b6c10f367e3d4bdb
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-11-10 21:49:05 +0100

    supabase setup and working

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://qsrpcpslrrmticsafiqa.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzcnBjcHNscnJtdGljc2FmaXFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyNjc4NzEsImV4cCI6MjA0Njg0Mzg3MX0._4hmLapIU_3X8xVjPf0TN_Rsfe7DB3UkclqyuYKGOdM";
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
