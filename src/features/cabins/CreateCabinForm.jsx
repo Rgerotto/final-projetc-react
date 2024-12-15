34ed9260e01211c6fc4899d71dc3f742743de907
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-12-15 11:07:55 +0100

    updating Modal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditeCabin";
 
-function CreateCabinForm({cabinToEdit = {}}) {
+function CreateCabinForm({cabinToEdit = {}, onCloseModal}) {
   const {isCreating, createCabin} = useCreateCabin();
   const {isEditing, editCabin} = useEditCabin();
 
@@ -28,12 +28,18 @@ function CreateCabinForm({cabinToEdit = {}}) {
     if(isEditSession)
       editCabin({newCabinData: {...data, image: image}, id: editId},
     {
-        onSuccess: (data) => reset(),
+        onSuccess: (data) => {
+          reset();
+          onCloseModal?.();
+        }
       }
     );
     else {
       createCabin({...data, image: image}, {
-        onSuccess: (data) => reset(),
+        onSuccess: (data) => {
+          reset();
+          onCloseModal?.();
+        }
       });
     };
   }
@@ -41,7 +47,7 @@ function CreateCabinForm({cabinToEdit = {}}) {
     //console.log(errors)
   }
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)} type={onCloseModal ? 'modal' : 'regular'}>
       <FormRow label='Cabin name' error={errors?.name?.message}>
         <Input type="text" id="name" disabled={isWorking} {...register(
           'name', {
@@ -102,7 +108,7 @@ function CreateCabinForm({cabinToEdit = {}}) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" onClick={() => onCloseModal?.()}>
           Cancel
         </Button>
         <Button disabled={isWorking}>{isEditSession ? 'Edit cabin' : 'Create new cabin'}</Button>
