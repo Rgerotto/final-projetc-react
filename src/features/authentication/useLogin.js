da045ff77a3ab7f592d21a64063d82fb1195e43d
Rafael Gerotto <rafael.gerotto@hotmail.com>
2025-01-01 11:31:39 +0100

    create new user and confirmation email has been sent

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -4,14 +4,14 @@ import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin () {
-    //const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
     const navigate = useNavigate();
     const {mutate: login, isLoading} = useMutation({
         mutationFn: ({email, password}) => loginApi({
             email, password
         }),
         onSuccess: (user) => {
-            //queryClient.setQueryData(['user'], user)
+            queryClient.setQueryData(['user'], user.user)
             //console.log(user)
             navigate('/dashboard', {replace: true});
         },
